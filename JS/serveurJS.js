eda956e1395e926e89287763b16913ad98bbf635
babyfrederic <62266289+babyfrederic@users.noreply.github.com>
2020-12-23 08:46:53 +0100

    mercre

---
+++ JS/serveurJS.js
@@ -0,0 +1,8 @@
+const navigation = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >30){
+        navigation.classList.add('anim-nav)');
+    } else {
+        navigation.classList.remove('anim-nav)');
+    }
